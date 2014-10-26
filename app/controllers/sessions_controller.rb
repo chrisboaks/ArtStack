df994f111439a4872e4b29dfcca57dc1df22bb73
Christopher Boaks <chris.boaks@gmail.com>
2014-10-25 22:38:51 -0400

    Add basic user/session logic

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+    if user
+      login(user)
+      redirect_to user_url(user)
+    else
+      flash.now[:errors] = ["There was a problem with that username/password combination.  Please try again."]
+    end
+  end
+
+  def destroy
+    sign_out
+    redirect_to new_session_url
+  end
+
+end
+
