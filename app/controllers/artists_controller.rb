7eccbeb8026cfcf1ab4584f61c6e3cd9c9ff8e85
App Academy Student <student@appacademy.io>
2014-10-28 15:31:40 -0400

    Flesh out views

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,7 +1,13 @@
 class ArtistsController < ApplicationController
 
   def show
-    @artist = Artist.find(params[:artist_id])
+    @artist = Artist.find(params[:id])
+    render :show
+  end
+
+  def index
+    @artists = Artist.all
+    render :index
   end
 
 end
