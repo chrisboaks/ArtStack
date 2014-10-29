9a62be8f26eb170f4b83f55144424aa946114035
Christopher Boaks <chris.boaks@gmail.com>
2014-10-29 17:03:54 -0400

    Cleanup routes

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,28 +1,28 @@
 class UserProfilesController < ApplicationController
 
-  def new
-    @user_profile = UserProfile.new
-    render :new
-  end
-
-  def create
-    @user_profile = UserProfile.new(user_profile_params)
-    @user_profile.user = current_user
-    if @user_profile.save
-      redirect_to user_url(@user_profile.user_id)
-    else
-      flash[:errors] = @user_profile.errors.full_messages
-      render :new
-    end
-  end
+  # def new
+  #   @user_profile = UserProfile.new
+  #   render :new
+  # end
+  #
+  # def create
+  #   @user_profile = UserProfile.new(user_profile_params)
+  #   @user_profile.user = current_user
+  #   if @user_profile.save
+  #     redirect_to user_url(@user_profile.user_id)
+  #   else
+  #     flash[:errors] = @user_profile.errors.full_messages
+  #     render :new
+  #   end
+  # end
 
   def edit
-    @user_profile = UserProfile.find_by(user: current_user)
+    @user_profile = UserProfile.find_or_initialize_by(user: current_user)
     render :edit
   end
 
   def update
-    @user_profile = UserProfile.find_by(user: current_user)
+    @user_profile = UserProfile.find_or_initialize_by(user: current_user)
     if @user_profile.update(user_profile_params)
       redirect_to user_url(@user_profile.user_id)
     else
