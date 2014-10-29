7467c7be5bfe3956d979bd6c28020892923f339c
Christopher Boaks <chris.boaks@gmail.com>
2014-10-29 11:53:57 -0400

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,9 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+me = User.create({username: "chrisb", password: "chrisb"})
+
 User.create([
-  {username: "chrisb", password: "chrisb"},
   {username: "picasso", password: "picasso"},
   {username: "lewitt", password: "lewitt"},
   {username: "duschamp", password: "duschamp"}])
@@ -18,12 +19,12 @@ johns = Artist.create(name: 'Jasper Johns')
 lewitt = Artist.create(name: 'Sol Lewitt')
 
 Artwork.create([
-  {image: 'http://erikaunapuu.files.wordpress.com/2013/05/picasso-girl-before-a-mirror.jpeg', uploader_id: 1, artist: picasso, title: 'Girl Before a Mirror', art_type: 'painting'},
-  {image: 'http://cp91279.biography.com/1000509261001/1000509261001_1910637818001_TDIH-Picasso-Guernica.jpg', uploader_id: 1, artist: picasso, title: 'Guernica', art_type: 'painting'},
-  {image: 'http://upload.wikimedia.org/wikipedia/commons/c/ce/Marcel_Duchamp.jpg', uploader_id: 1, artist_id: duschamp, title: 'Fountain', art_type: 'sculpture'},
-  {image: 'http://upload.wikimedia.org/wikipedia/en/c/c0/Duchamp_-_Nude_Descending_a_Staircase.jpg', uploader_id: 1, artist: duschamp, title: 'Nude Descending a Staircase', art_type: 'painting'},
-  {image: 'http://www.moma.org/collection_images/resized/377/w500h420/CRI_136377.jpg', uploader_id: 1, artist: johns, title: 'Flag', art_type: 'painting'},
-  {image: 'http://www.tate.org.uk/art/images/work/T/T00/T00454_10.jpg', uploader_id: 1, artist: johns, title: '0-9', art_type: 'painting'},
-  {image: 'http://www.blackbird.vcu.edu/v7n1/gallery/ravenal_j/images/lewitt17.jpg', uploader_id: 1, artist: lewitt, title: 'Splotch #22', art_type: 'sculpture'},
-  {image: 'http://thenevicaproject.org/Gallery%20Artist/popups/lewitt_distorted_cubes.jpg', uploader_id: 1, artist: lewitt, title: 'Distorted Cubes', art_type: 'painting'}
+  {image: 'http://erikaunapuu.files.wordpress.com/2013/05/picasso-girl-before-a-mirror.jpeg', uploader: me, artist: picasso, title: 'Girl Before a Mirror', art_type: 'painting'},
+  {image: 'http://cp91279.biography.com/1000509261001/1000509261001_1910637818001_TDIH-Picasso-Guernica.jpg', uploader: me, artist: picasso, title: 'Guernica', art_type: 'painting'},
+  {image: 'http://upload.wikimedia.org/wikipedia/commons/c/ce/Marcel_Duchamp.jpg', uploader: me, artist_id: duschamp, title: 'Fountain', art_type: 'sculpture'},
+  {image: 'http://upload.wikimedia.org/wikipedia/en/c/c0/Duchamp_-_Nude_Descending_a_Staircase.jpg', uploader: me, artist: duschamp, title: 'Nude Descending a Staircase', art_type: 'painting'},
+  {image: 'http://www.moma.org/collection_images/resized/377/w500h420/CRI_136377.jpg', uploader: me, artist: johns, title: 'Flag', art_type: 'painting'},
+  {image: 'http://www.tate.org.uk/art/images/work/T/T00/T00454_10.jpg', uploader: me, artist: johns, title: '0-9', art_type: 'painting'},
+  {image: 'http://www.blackbird.vcu.edu/v7n1/gallery/ravenal_j/images/lewitt17.jpg', uploader: me, artist: lewitt, title: 'Splotch #22', art_type: 'sculpture'},
+  {image: 'http://thenevicaproject.org/Gallery%20Artist/popups/lewitt_distorted_cubes.jpg', uploader: me, artist: lewitt, title: 'Distorted Cubes', art_type: 'painting'}
 ])
