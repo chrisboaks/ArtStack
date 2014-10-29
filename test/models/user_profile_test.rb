1592173dae29a643e50a555acfb4454900d71da0
App Academy Student <student@appacademy.io>
2014-10-29 15:50:35 -0400

    Create working new user profile form

--- test/models/user_profile_test.rb
+++ test/models/user_profile_test.rb
@@ -2,23 +2,23 @@
 #
 # Table name: user_profiles
 #
-#  id               :integer          not null, primary key
-#  username         :text
-#  first_name       :text
-#  last_name        :text
-#  location         :text
-#  bio              :text
-#  artist           :boolean
-#  art_professional :boolean
-#  collector        :boolean
-#  art_lover        :boolean
-#  curator          :boolean
-#  art_writer       :boolean
-#  student          :boolean
-#  website          :text
-#  user_id          :integer
-#  created_at       :datetime
-#  updated_at       :datetime
+#  id                  :integer          not null, primary key
+#  username            :text
+#  first_name          :text
+#  last_name           :text
+#  location            :text
+#  bio                 :text
+#  is_artist           :boolean
+#  is_art_professional :boolean
+#  is_collector        :boolean
+#  is_art_lover        :boolean
+#  is_curator          :boolean
+#  is_art_writer       :boolean
+#  is_student          :boolean
+#  website             :text
+#  user_id             :integer
+#  created_at          :datetime
+#  updated_at          :datetime
 #
 
 require 'test_helper'
