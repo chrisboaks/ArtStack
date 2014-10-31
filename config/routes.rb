62310b78b1c93193554d8916eb7479b7192f81a0
Christopher Boaks <chris.boaks@gmail.com>
2014-10-31 15:41:59 -0400

    Reannotate

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # == Route Map
 #
 #                  Prefix Verb   URI Pattern                                  Controller#Action
-#                    root GET    /                                            sessions#new
+#                    root GET    /                                            artworks#index
 #  auth_facebook_callback GET    /auth/facebook/callback(.:format)            oauthcallbacks#facebook
 #                sessions POST   /sessions(.:format)                          sessions#create
 #             new_session GET    /sessions/new(.:format)                      sessions#new
@@ -19,8 +19,14 @@
 #                 artwork GET    /artworks/:id(.:format)                      artworks#show
 #                 artists GET    /artists(.:format)                           artists#index
 #                  artist GET    /artists/:id(.:format)                       artists#show
-#                  stacks POST   /stacks(.:format)                            stacks#create
-#                   stack DELETE /stacks/:id(.:format)                        stacks#destroy
+#            api_artworks GET    /api/artworks(.:format)                      api/artworks#index {:format=>:json}
+#                         POST   /api/artworks(.:format)                      api/artworks#create {:format=>:json}
+#         new_api_artwork GET    /api/artworks/new(.:format)                  api/artworks#new {:format=>:json}
+#             api_artwork GET    /api/artworks/:id(.:format)                  api/artworks#show {:format=>:json}
+#             api_artists GET    /api/artists(.:format)                       api/artists#index {:format=>:json}
+#              api_artist GET    /api/artists/:id(.:format)                   api/artists#show {:format=>:json}
+#              api_stacks POST   /api/stacks(.:format)                        api/stacks#create {:format=>:json}
+#               api_stack DELETE /api/stacks/:id(.:format)                    api/stacks#destroy {:format=>:json}
 #
 
 Rails.application.routes.draw do
