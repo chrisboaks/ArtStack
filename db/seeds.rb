d552b92b4247b3c32631bae3cf0095ec9415dc50
Christopher Boaks <chris.boaks@gmail.com>
2014-11-01 23:24:49 -0400

    Remove vestiges of column rename

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,12 @@ johns = Artist.create(name: 'Jasper Johns')
 lewitt = Artist.create(name: 'Sol Lewitt')
 
 Artwork.create([
-  {image_url: 'http://erikaunapuu.files.wordpress.com/2013/05/picasso-girl-before-a-mirror.jpeg', uploader: me, artist: picasso, title: 'Girl Before a Mirror', art_type: 'painting'},
-  {image_url: 'http://cp91279.biography.com/1000509261001/1000509261001_1910637818001_TDIH-Picasso-Guernica.jpg', uploader: me, artist: picasso, title: 'Guernica', art_type: 'painting'},
-  {image_url: 'http://upload.wikimedia.org/wikipedia/commons/c/ce/Marcel_Duchamp.jpg', uploader: me, artist: duschamp, title: 'Fountain', art_type: 'sculpture'},
-  {image_url: 'http://upload.wikimedia.org/wikipedia/en/c/c0/Duchamp_-_Nude_Descending_a_Staircase.jpg', uploader: me, artist: duschamp, title: 'Nude Descending a Staircase', art_type: 'painting'},
-  {image_url: 'http://www.moma.org/collection_images/resized/377/w500h420/CRI_136377.jpg', uploader: me, artist: johns, title: 'Flag', art_type: 'painting'},
-  {image_url: 'http://www.tate.org.uk/art/images/work/T/T00/T00454_10.jpg', uploader: me, artist: johns, title: '0-9', art_type: 'painting'},
-  {image_url: 'http://www.blackbird.vcu.edu/v7n1/gallery/ravenal_j/images/lewitt17.jpg', uploader: me, artist: lewitt, title: 'Splotch #22', art_type: 'sculpture'},
-  {image_url: 'http://thenevicaproject.org/Gallery%20Artist/popups/lewitt_distorted_cubes.jpg', uploader: me, artist: lewitt, title: 'Distorted Cubes', art_type: 'painting'}
+  {image: 'http://erikaunapuu.files.wordpress.com/2013/05/picasso-girl-before-a-mirror.jpeg', uploader: me, artist: picasso, title: 'Girl Before a Mirror', art_type: 'painting'},
+  {image: 'http://cp91279.biography.com/1000509261001/1000509261001_1910637818001_TDIH-Picasso-Guernica.jpg', uploader: me, artist: picasso, title: 'Guernica', art_type: 'painting'},
+  {image: 'http://upload.wikimedia.org/wikipedia/commons/c/ce/Marcel_Duchamp.jpg', uploader: me, artist: duschamp, title: 'Fountain', art_type: 'sculpture'},
+  {image: 'http://upload.wikimedia.org/wikipedia/en/c/c0/Duchamp_-_Nude_Descending_a_Staircase.jpg', uploader: me, artist: duschamp, title: 'Nude Descending a Staircase', art_type: 'painting'},
+  {image: 'http://www.moma.org/collection_images/resized/377/w500h420/CRI_136377.jpg', uploader: me, artist: johns, title: 'Flag', art_type: 'painting'},
+  {image: 'http://www.tate.org.uk/art/images/work/T/T00/T00454_10.jpg', uploader: me, artist: johns, title: '0-9', art_type: 'painting'},
+  {image: 'http://www.blackbird.vcu.edu/v7n1/gallery/ravenal_j/images/lewitt17.jpg', uploader: me, artist: lewitt, title: 'Splotch #22', art_type: 'sculpture'},
+  {image: 'http://thenevicaproject.org/Gallery%20Artist/popups/lewitt_distorted_cubes.jpg', uploader: me, artist: lewitt, title: 'Distorted Cubes', art_type: 'painting'}
 ])
