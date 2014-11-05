fb684f457d1cad5bd4eca0e44ff1c17343e4dad0
App Academy Student <appacademy@Apps-Mac-mini-5.local>
2014-11-04 21:51:37 -0500

    Make artworks index and artist show prettier

--- app/assets/javascripts/views/artists/artist_show.js
+++ app/assets/javascripts/views/artists/artist_show.js
@@ -15,14 +15,41 @@ ArtStack.Views.ArtistShow = Backbone.View.extend({
     var renderedContent = this.template({ artist: this.model });
     this.$el.html(renderedContent);
     var that = this;
+    var uls = $('ul.artist-show-list').toArray();
+
+
+// old
+    // if (this.model.artworks) {
+    //   this.model.artworks.each(function (artwork) {
+    //     var view = new ArtStack.Views.ArtistShowArtworkLI({model: artwork});
+    //     that.subviews.push(view);
+    //     that.$el.find("#" + that.model.id).append(view.render().$el);
+    //   });
+    // }
+
+// new
+    // this.collection.each(function (artwork) {
+    //
+    //   var view = new ArtStack.Views.ArtworksIndexArtworkLI({ model: artwork });
+    //   that.subviews.push(view);
+    //
+    //   var lowest = uls.reduce(function (a, b) {
+    //     return $(a).height() < $(b).height() ? a : b;
+    //   });
+    //
+    //   $(lowest).append(view.render().$el);
+    // });
+
 
     if (this.model.artworks) {
       this.model.artworks.each(function (artwork) {
         var view = new ArtStack.Views.ArtistShowArtworkLI({model: artwork});
         that.subviews.push(view);
-        that.$el.find("#" + that.model.id).append(view.render().$el);
+
+        var shortest = ($(uls[0]).height() < $(uls[1]).height() ? uls[0] : uls[1]);
+        $(shortest).append(view.render().$el);
       });
-    }
+    };
 
     return this;
   },
