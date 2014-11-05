58945e59b76952937d950187830990bae461715e
Christopher Boaks <chris.boaks@gmail.com>
2014-11-05 15:18:29 -0500

    CSS Cleanup, install Droid Sans

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,10 @@ module ArtStack
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
+
+    config.assets.precompile += %w(DroidSans.ttf)
+
     config.paperclip_defaults = {
       :storage => :s3,
       :s3_protocol => 'http',
