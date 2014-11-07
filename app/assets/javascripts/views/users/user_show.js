676c7cbd305037c85f4fded4de27e6277aa80ee2
Christopher Boaks <chris.boaks@gmail.com>
2014-11-07 11:26:47 -0500

    Follow users

--- app/assets/javascripts/views/users/user_show.js
+++ app/assets/javascripts/views/users/user_show.js
@@ -4,20 +4,29 @@ ArtStack.Views.UserShow = Backbone.View.extend({
 
   profileTemplate: JST['users/_user_profile'],
 
+  followTemplate: JST['follows/_follow_button'],
+
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
     this.subviews = [];
     this.ul_lengths = [0, 0];
   },
 
+  events: {
+    "click button.follow-button": "toggleFollow"
+  },
+
   render: function () {
     var renderedContent = this.template({ user: this.model });
     var profileContent = this.profileTemplate({ user: this.model });
+    
+    var followButton = this.followTemplate({ followable: this.model });
 
     this.$el.html(renderedContent);
     this.$el.find('#user-profile').append(profileContent);
-    that = this;
+    this.$el.prepend(followButton);
 
+    that = this;
     if (this.model.user_artworks) {
       this.model.user_artworks.each(function (artwork) {
         that.addArtwork(artwork);
@@ -36,6 +45,39 @@ ArtStack.Views.UserShow = Backbone.View.extend({
 
     $(uls[shortest_ul_index]).prepend(view.render().$el);
     that.ul_lengths[shortest_ul_index] += artwork.get('height');
+  },
+
+  toggleFollow: function (event) {
+    event.preventDefault();
+
+    if (this.model.get('followed')) {
+
+      $("button.follow-button").addClass("followed-false");
+      $("button.follow-button").removeClass("followed-true");
+      this.model.set({ followed: false });
+
+      $.ajax({
+        type: "PATCH",
+        url: "/api/users/" + this.model.id,
+        data: {
+          follow: false
+        }
+      });
+
+    } else {
+
+      $("button.follow-button").addClass("followed-true");
+      $("button.follow-button").removeClass("followed-false");
+      this.model.set({ followed: true });
+
+      $.ajax({
+        type: "PATCH",
+        url: "/api/users/" + this.model.id,
+        data: {
+          follow: true
+        }
+      });
+    }
   },
 
 });
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -12,4 +12,17 @@ class Api::UsersController < ApplicationController
     render :show
   end
 
+  def update
+    @user = User.find(params[:id])
+    to_follow = params[:follow]
+
+    if to_follow == 'true'
+      Follow.create(follower: current_user, followable: @user)
+    else
+      follow = Follow.find_by(follower: current_user, followable: @user)
+      follow.destroy
+    end
+    render :show
+  end
+
 end
