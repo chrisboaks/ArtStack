db9486c1516b2ec84d996a43c1aaefea491d0859
Christopher Boaks <chris.boaks@gmail.com>
2014-11-07 11:38:04 -0500

    Fix stack button refresh bug

--- app/assets/javascripts/views/artworks/_medium_artwork_li.js
+++ app/assets/javascripts/views/artworks/_medium_artwork_li.js
@@ -28,8 +28,8 @@ ArtStack.Views.MediumArtworkLI = Backbone.View.extend({
 
     if (this.model.get('stacked')) {
 
-      $("button").addClass("stacked-false");
-      $("button").removeClass("stacked-true");
+      this.$("button").addClass("stacked-false");
+      this.$("button").removeClass("stacked-true");
       this.model.set({ stacked: false });
 
       $.ajax({
@@ -42,8 +42,8 @@ ArtStack.Views.MediumArtworkLI = Backbone.View.extend({
 
     } else {
 
-      $("button").addClass("stacked-true");
-      $("button").removeClass("stacked-false");
+      this.$("button").addClass("stacked-true");
+      this.$("button").removeClass("stacked-false");
       this.model.set({ stacked: true });
 
       $.ajax({
