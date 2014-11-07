6f24a57a5c205a4481adc9fdad35d1bb3bb7e3b5
Christopher Boaks <chris.boaks@gmail.com>
2014-11-07 11:47:17 -0500

    Squash some bugs

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -3,4 +3,13 @@ class Follow < ActiveRecord::Base
   belongs_to :followable, :polymorphic => true
 
   validates :follower_id, :uniqueness => { :scope => [:followable_id, :followable_type]}
+
+  validate :check_for_self_follow
+
+  def check_for_self_follow
+    if follower_id == followable_id && followable_type == "User"
+      errors.add(:follow, "can't follow yourself")
+    end
+  end
+
 end
